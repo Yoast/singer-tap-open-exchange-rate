3360a9fab6e867f36d7844503a23026272c40134
brent-yoast <brent@yoast.com>
2022-05-19 08:24:12 +0200

    added required install to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         'httpx[http2]~=0.16.1',
         'python-dateutil~=2.8.1',
         'singer-python~=5.10.0',
+        'requests~=2.27.1',
     ],
     entry_points="""
         [console_scripts]
